26d1765ead9aee265116fe8d4fd204081dec8aa3
PBeGood4 <pedrocbeirao@gmail.com>
2022-11-09 10:31:02 +0000

    Fix windows versiono hanging when using the help command

--- dsda-launcher/mainwindow.cpp
+++ dsda-launcher/mainwindow.cpp
@@ -2034,8 +2034,11 @@ void MainWindow::on_toolButton_3_clicked()
 #ifdef __APPLE__
             QProcess *process = new QProcess;
             process->startDetached("sh", {"-c", "echo \""+path+" --help ; rm /tmp/dsda-doom-params.sh\" > /tmp/dsda-doom-params.sh ; chmod +x /tmp/dsda-doom-params.sh ; open -a Terminal /tmp/dsda-doom-params.sh"});
+            process->deleteLater();
 #elif _WIN32
-            system(("cmd /k "+path.toStdString()+" --help").c_str());
+            QProcess *process = new QProcess;
+            process->startDetached("start", {"cmd.exe", "@cmd", "/k", path.toStdString()+" --help"});
+            process->deleteLater();
 #else
             // xterm is the most reliable terminal to use, but it seems a few distros do not have it
             system(("xterm -e 'bash -c \""+path.toStdString()+" --help ;bash\"'").c_str());
