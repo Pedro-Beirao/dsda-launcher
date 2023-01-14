462af4812071342832def54c1653d7c2decc4f22
PBeGood4 <pedrocbeirao@gmail.com>
2023-01-14 11:27:44 +0000

    Rename Console objects

--- dsda-launcher/console.cpp
+++ dsda-launcher/console.cpp
@@ -25,13 +25,13 @@ Console::~Console()
 
 void Console::changeText(std::string s)
 {
-    QString previousText = ui->text->toPlainText();
-    ui->text->setPlainText(previousText + s.c_str());
+    QString previousText = ui->console_plainTextEdit->toPlainText();
+    ui->console_plainTextEdit->setPlainText(previousText + s.c_str());
 }
 
 void Console::clearText()
 {
-    ui->text->setPlainText("");
+    ui->console_plainTextEdit->setPlainText("");
 }
 
 
