94b3c5151536c7677ea50b0c96f2fd7217d976cf
Pedro Beirao <pedrocbeirao@gmail.com>
2024-09-24 22:48:45 +0100

    Use the correct color codes for endoom
    TODO
    - Get the correct character styles
    - Fix weird lines/artifacts (eviternity 2 for example)

--- src/endoom.h
+++ src/endoom.h
@@ -16,30 +16,29 @@ class endoom : public QWidget
 
 private:
     // I am still confussed with what doomwiki says about the colors
-    std::unordered_map<QString, QString> colors =
-    {
-        {"0m", "black"},
-        {"1m", "darkred"},
-        {"2m", "green"},
-        {"3m", "yellow"},
-        {"4m", "blue"},
-        {"5m", "purple"},
-        {"6m", "cyan"},
-        {"7m", "whitesmoke"},
-        {"8m", "gray"},
-        {"9m", "white"},
+  std::unordered_map<QString, QString> colors = {
+      {"0m", "#000000"}, // Black
+      {"1m", "#aa0000"}, // Red
+      {"2m", "#00aa00"}, // Green
+      {"3m", "#aa5500"}, // Yellow
+      {"4m", "#0000aa"}, // Blue
+      {"5m", "#aa00aa"}, // Magenta
+      {"6m", "#00aaaa"}, // Cyan
+      {"7m", "#aaaaaa"}, // White
+      {"8m", "white"},   //
+      {"9m", "white"},   //
 
-        {"0;", "gray"},
-        {"1;", "indianred "},
-        {"2;", "lawngreen"},
-        {"3;", "gold"},
-        {"4;", "#6871ff"},
-        {"5;", "magenta"},
-        {"6;", "lightcyan"},
-        {"7;", "white"},
-        {"8;", "lightgray"},
-        {"9;", "white"},
-    };
+      {"0;", "#555555"},   // Bright Black
+      {"1;", "#ff5555 "},  // Bright Red
+      {"2;", "#55ff55"},   // Bright Green
+      {"3;", "#ffff55"},   // Bright Yellow
+      {"4;", "#5555ff"},   // Bright Blue
+      {"5;", "#ff55ff"},   // Bright Magenta
+      {"6;", "#55ffff"},   // Bright Cyan
+      {"7;", "#ffffff"},   // Bright White
+      {"8;", "lightgray"}, //
+      {"9;", "white"},     //
+  };
 
 public:
     explicit endoom(QWidget *parent = nullptr);
