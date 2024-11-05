789be294ddc65b8ee29a466e3e1faa7fca82c721
Pedro Beirao <pedrocbeirao@gmail.com>
2024-11-05 14:36:38 +0000

    Better dialogs for updating the game

--- src/mainwindow_actions.cpp
+++ src/mainwindow_actions.cpp
@@ -55,83 +55,10 @@ void MainWindow::on_actionCheckForUpdatesDsdadoom_triggered()
         return;
     }
 
-    QString portversion;
+    QString current = getGameVersion();
+    QString latest = getLatestGameVersion();
 
-    QString path = getGamePath();
-
-    QFile port = QFile(path);
-    if (port.exists())
-    {
-        QProcess *process = new QProcess;
-        process->setWorkingDirectory(QStandardPaths::writableLocation(QStandardPaths::HomeLocation));
-        process->start(path, {"-v"});
-        process->waitForFinished();
-
-        QList output = process->readAll().split(' ');
-
-        if (output.size() >= 2) portversion = output[1];
-    }
-
-    QNetworkRequest req0((QUrl(GAME_API)));
-    req0.setHeader(QNetworkRequest::ContentTypeHeader, "application/json");
-    QJsonObject json0;
-    QNetworkAccessManager nam0;
-    QNetworkReply *reply0 = nam0.get(req0);
-    while (!reply0->isFinished())
-    {
-        qApp->processEvents();
-    }
-    QByteArray response_data = reply0->readAll();
-    QJsonDocument jsondoc = QJsonDocument::fromJson(response_data);
-    QJsonObject jsonobj = jsondoc.object();
-    foreach (const QString &key, jsonobj.keys())
-    {
-        QJsonValue value = jsonobj.value(key);
-        if (key == "name")
-        {
-            if (portversion != value.toString())
-            {
-                QMessageBox msgBox;
-                msgBox.setText("DSDA-Doom " + portversion);
-                msgBox.setInformativeText("Available: " + value.toString());
-                QPushButton *pButtonYes;
-                pButtonYes = msgBox.addButton(tr("Update"), QMessageBox::YesRole);
-                msgBox.addButton(tr("Ignore"), QMessageBox::NoRole);
-                msgBox.setDefaultButton(pButtonYes);
-                msgBox.exec();
-                if (msgBox.clickedButton() == pButtonYes)
-                {
-#if defined(Q_OS_MAC)
-                    QProcess *process = new QProcess;
-                    // clang-format off
-                    process->startDetached("sh", {"-c", "rm /tmp/dsda-updater-macos.sh;"
-                                                        "curl -L -o /tmp/dsda-updater-macos.sh " + GAME_UPDATER_MACOS + ";"
-                                                        "p2=" + launcherfolder + "/../Resources;"
-                                                        "p2=${p2//\\//\\\\/};"
-                                                        "sed -i -e s/'$1'/$p2/g /tmp/dsda-updater-macos.sh;"
-                                                        "chmod +x /tmp/dsda-updater-macos.sh;"
-                                                        "open -a Terminal --args /tmp/dsda-updater-macos.sh"});
-                    // clang-format on
-                    process->deleteLater();
-#elif defined(Q_OS_WIN)
-                    QDesktopServices::openUrl(QUrl(GAME_REPO));
-#elif defined(Q_OS_LINUX)
-                    QDesktopServices::openUrl(QUrl(GAME_REPO));
-#endif
-                }
-            }
-            else
-            {
-                QMessageBox msgBox;
-                msgBox.setText("DSDA-Doom " + portversion);
-                msgBox.setInformativeText("Up to Date");
-                msgBox.addButton(tr("Ignore"), QMessageBox::NoRole);
-                msgBox.exec();
-            }
-        }
-    }
-
-    reply0->deleteLater();
+    updateGameDialog(current, latest);
 }
 
 void MainWindow::on_actionOpenSettings_triggered()
